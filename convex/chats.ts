8d97c07f3633974d8eeffa1b05ece843e83de2ba
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-08 15:11:59 +0100

    get chat

--- convex/chats.ts
+++ convex/chats.ts
@@ -88,3 +88,55 @@ export const listByCurrentUser = query({
     },
 });
 
+
+export const getChat = query({
+    args: { chatId: v.id("chats") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        // current user
+        const currentUser = await ctx.db
+            .query("users")
+            .withIndex("by_token", (q) =>
+                q.eq("tokenIdentifier", identity.tokenIdentifier)
+            )
+            .unique();
+        if (!currentUser) {
+            throw new Error("Couldn't authenticate user");
+        }
+
+
+        const chat = await ctx.db.get(args.chatId);
+
+        if (!chat) {
+            throw new ConvexError("Chat not found");
+        }
+
+        const messages = await ctx.db
+            .query("messages")
+            .withIndex("by_chatId", (q) => q.eq("chatId", chat._id))
+            .collect();
+
+        const messagesWithUsersRelation = messages.map(async (message: Doc<"messages">) => {
+            const user = await ctx.db.get(message.authorId);
+            if (!user) {
+                throw new ConvexError("User doesn't exist");
+            }
+            return {
+                ...message,
+                user
+            }
+        });
+
+        const messagesWithUsers = await Promise.all(messagesWithUsersRelation);
+
+        return {
+            chat,
+            messagesWithUsers
+        };
+    }
+});
