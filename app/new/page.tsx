cf83a1d437ab425dd8ad0d863acb17c0e50d5812
tomtarpeyyt <ttarpey58@gmail.com>
2024-04-08 03:02:29 +0100

    Linked up clerk auth to convex to store users & created schemas

---
+++ app/new/page.tsx
@@ -0,0 +1,8 @@
+export default function New() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        time2code New Page
+      </main>
+    );
+  }
+  
